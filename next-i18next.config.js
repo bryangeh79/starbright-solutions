48e3d844d4bb3893f8a25419bf48443b42f73bbc
bryangeh79 <bryangeh79@gmail.com>
2026-04-09 17:01:21 +0800

    Add next-i18next.config.js and public/locales/ (were untracked, missing from Vercel)

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-i18next').UserConfig} */
+module.exports = {
+  i18n: {
+    defaultLocale: 'zh',
+    locales: ['zh', 'en', 'ms', 'vi', 'id', 'th'],
+  },
+  localePath: './public/locales',
+  fallbackLng: 'en',
+};
